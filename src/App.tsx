53ce47058ca74de5269275c028f96e07b7ba6fa6
Lucas Alves <lucasalves94@gmail.com>
2023-01-18 21:29:18 -0300

    styled components applied to buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import Button from "./components/Button"
+
+
+
 export function App() {
 
   return (
-    <h1>Hello my friends</h1>
+    <>
+    <Button variant='primary'/>
+    <Button variant='secondary'/>
+    <Button variant='success'/>
+    <Button variant='danger'/>
+    <Button />
+    </>
   )
 }
 
