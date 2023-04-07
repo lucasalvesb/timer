7cb168ed43ba444694d1dbd6f771b1fb6ca3cb51
Lucas Alves <lucasalves94@gmail.com>
2023-04-07 16:59:21 -0300

    added home folder and basic structure

--- Router.tsx
+++ Router.tsx
@@ -7,7 +7,10 @@ import { Home } from './src/pages/Home'
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<DefaultLayout />}>
+      <Route
+        path="/"
+        element={<DefaultLayout />}
+      >
         <Route
           path="/"
           element={<Home />}
