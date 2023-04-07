3f65d63042b8ad6f37b34819916628ca99584a6c
Lucas Alves <lucasalves94@gmail.com>
2023-04-07 17:05:12 -0300

    basic clock info added

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,27 @@
 export function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <form action="">
+        <label htmlFor="task">Vou trabalhar em </label>
+        <input id="task" />
+
+        <label htmlFor="">durante</label>
+        <input
+          type="number"
+          id="minutesAmount"
+        />
+
+        <span>minutos.</span>
+
+        <div>
+          <span>0</span>
+          <span>0</span>
+          <span>:</span>
+          <span>0</span>
+          <span>0</span>
+        </div>
+        <button type="submit"></button>
+      </form>
     </div>
   )
 }
